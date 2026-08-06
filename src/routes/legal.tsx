5ae0bab4d335b4f5298d69942236e27a69ea5db6
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-08-06 10:39:10 +0800

    Role-aware dashboard routing

--- src/routes/legal.tsx
+++ src/routes/legal.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 
 import { PublicLayout, PageHero } from "@/components/site/public-layout";
@@ -66,6 +66,7 @@ function LegalCenterPage() {
           </div>
         )}
       </section>
+      <Outlet />
     </PublicLayout>
   );
 }
